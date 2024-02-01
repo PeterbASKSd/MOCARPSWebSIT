161a7e8f6d981e8f5bdd3e8ab3f421a067aa6dd3
PeterbASKSd <92280638+PeterbASKSd@users.noreply.github.com>
2024-02-01 17:26:44 +0800

    Update

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -13,6 +13,7 @@ import "katex/dist/katex.min.css";
 window.katex = katex;
 import { Editor } from "@tinymce/tinymce-react";
 import "@peterbasksd/tinymce-mathjax";
+import "tinymce/tinymce";
 
 type Props = {
   slug: string;
