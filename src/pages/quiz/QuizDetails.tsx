f81509ce8380baa079c07bef743bf9e094ed5673
PeterbASKSd <92280638+PeterbASKSd@users.noreply.github.com>
2024-05-27 10:10:50 +0800

    Update

--- src/pages/quiz/QuizDetails.tsx
+++ src/pages/quiz/QuizDetails.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
-import "./QuizDetails.scss";
+import "../quiz/quizDetails.scss";
 import {
   PieChart,
   Pie,
